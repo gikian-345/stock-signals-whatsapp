a13a41e754cbe8a357daeabdd377b74d435c50e7
gikian-345 <102648948+gikian-345@users.noreply.github.com>
2025-10-30 22:43:17 -0400

    Update messenger.py

--- app/messenger.py
+++ app/messenger.py
@@ -1,6 +1,8 @@
 import os
 from twilio.rest import Client
 
+from messenger_telegram import send_telegram as send_message
+
 def send_whatsapp_message(to_number: str, body: str) -> None:
     sid = os.environ["TWILIO_ACCOUNT_SID"]
     token = os.environ["TWILIO_AUTH_TOKEN"]
